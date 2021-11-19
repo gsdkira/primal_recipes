1c5ec23867bfa2ff252de4db807d3a829f72c2fc
gsdkira <larreakelly@gmail.com>
2021-11-19 12:11:47 -0500

    remigrated paleo model adjusting new information as well as able to render favorites to favorites page but as an object for now

--- models/paleorecipe.js
+++ models/paleorecipe.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.paleoRecipe.belongsToMany(models.user, {through: 'userPaleoRecipe'})
+      // models.paleoRecipe.belongsToMany(models.user, {through: 'userPaleoRecipe'})
     }
   };
   paleoRecipe.init({
---
+++ scratch.js
@@ -0,0 +1,69 @@
+// 'use strict';
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     await queryInterface.createTable('userKetoRecipes', {
+//       id: {
+//         allowNull: false,
+//         autoIncrement: true,
+//         primaryKey: true,
+//         type: Sequelize.INTEGER
+//       },
+//       userId: {
+//         type: Sequelize.INTEGER
+//       },
+//       ketoRecipeUrl: {
+//         type: Sequelize.STRING
+//       },
+//       ketoRecipeLabel: {
+//         type: Sequelize.STRING
+//       },
+//       createdAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       },
+//       updatedAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       }
+//     });
+//   },
+//   down: async (queryInterface, Sequelize) => {
+//     await queryInterface.dropTable('userKetoRecipes');
+//   }
+// };
+
+// 'use strict';
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     await queryInterface.createTable('userPaleoRecipes', {
+//       id: {
+//         allowNull: false,
+//         autoIncrement: true,
+//         primaryKey: true,
+//         type: Sequelize.INTEGER
+//       },
+//       userId: {
+//         type: Sequelize.INTEGER
+//       },
+//       paleoRecipeUrl: {
+//         type: Sequelize.STRING
+//       },
+//       paleoRecipeLabel: {
+//         type: Sequelize.STRING
+//       },
+//       createdAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       },
+//       updatedAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       }
+//     });
+//   },
+//   down: async (queryInterface, Sequelize) => {
+//     await queryInterface.dropTable('userPaleoRecipes');
+//   }
+// };
+
+
